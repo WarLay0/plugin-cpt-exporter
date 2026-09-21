e53555fa618de38a18c602972ba4f6367389203d
evan <evan.bombart.pro@gmail.com>
2026-09-21 10:51:55 +0200

    :feat: add French translation

--- plugin-cpt-exporter.php
+++ plugin-cpt-exporter.php
@@ -4,9 +4,16 @@
  * Description:      Exports custom post types.
  * Version:          0.0.1
  * Author:           Evan BOMBART
+ * Text Domain:      plugin-cpt-exporter
+ * Domain Path:      /languages
  */
 
 if (!defined('ABSPATH')) exit;
+
+// Outside wordpress.org, WordPress only looks for translations in wp-content/languages: point it to ours.
+add_action('init', function () {
+  load_plugin_textdomain('plugin-cpt-exporter', false, dirname(plugin_basename(__FILE__)) . '/languages');
+});
 
 require_once __DIR__ . '/includes/cpt-exporter-settings.php';
 require_once __DIR__ . '/includes/cpt-exporter-order.php';
